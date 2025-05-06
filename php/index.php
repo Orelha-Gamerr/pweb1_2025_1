704b1d9fb906639781d9c3b9179eed52e61d784e
Orelha-Gamerr <angelolucietto@gmail.com>
2025-05-06 08:23:33 -0300

    Aula dia 30

---
+++ php/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <style type="text/css">
+            @import url("style.css") all;
+        </style>
+    </head>
+    <body>
+       <?php
+        $nome = "Angelo";
+        echo "Bem vindo, $nome";
+
+        $idade = 18;
+        if($idade < 18){
+            echo " <br>Menor de Idade";
+        } else {
+            echo " <br>Maior de Idade";
+        }
+        echo "<br>";
+        $notas = [7, 6, 5, 8, 9];
+        for($i = 0; $i < count(value:$notas); $i++){
+            echo $notas[$i];
+        }
+
+       ?>
+    </body>
+</html>
